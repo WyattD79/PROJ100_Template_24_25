6103bd0f262404b641567f0469b69ac3a2cb105f
WyattD79 <wyatt.darley@students.plymouth.ac.uk>
2025-03-17 14:53:03 +0000

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -25,6 +25,141 @@ PROJ100_Encoder left_encoder(ENCODER_PIN_LEFT,PULSES_PER_ROTATION);     //Instan
 
 UnbufferedSerial ser(USBTX,USBRX,115200);   // Serial object for printing info
 
+bool switch1 = false;
+bool switch2 = false;
+
+
+void leftspeedcontroller(){
+
+    float left_target_rpm = 40.0f;
+    float right_target_rpm = 40.0f;       // What rpm are we going to try to achieve?
+    float pwm_increment = 0.003f;   // This value affects how quickly we can reach the target but also the stability of our controller
+    int loop_delay_ms = 1;          // This sets how often the loop runs
+    int ppr = 20;//left_encoder.getPulsesPerRotation();      // To work out how fast we are going we need to know how many pulses are in a complete rotation
+    
+    Timer left_print_timer;
+    Timer right_print_timer;
+    left_print_timer.start();
+
+    float left_last_rpm = 0.0f;
+    while(1){
+        int32_t left_time = left_encoder.getLastPulseTimeUs(); //check pulse
+        if(left_time>0){ //if pulse received
+            float left_rpm = (60000000.0f/(ppr*left_time));               //calculate rpm
+            float err = left_target_rpm-left_rpm;                         //calculate error rate
+            if(left_target_rpm==0.0f){           //check if target rpm is 0
+                Wheel.Speed(0.0f ,0.0f);     // If so stop the motors 
+            }
+            else{                                        //alter speed
+                if(left_rpm < (left_target_rpm)){
+                    Wheel.Speed(0.0f,Wheel.getSpeedLeft()+pwm_increment);
+                }
+                else if(left_rpm > left_target_rpm){
+                    Wheel.Speed(0.0f,Wheel.getSpeedLeft()-pwm_increment);
+                }
+            }  
+
+            left_last_rpm = left_rpm;
+        }
+ 
+        // Check to see if the wheel IS stationary
+        else if(left_time==-2){                      //check if wheel is stationary
+            left_last_rpm = 0.0f;        
+            if(left_target_rpm !=0.0f){
+                Wheel.Speed(0.0f,Wheel.getSpeedLeft()+pwm_increment);   //if stationary but needs to move increase speed
+            }
+            else{
+                Wheel.Speed(0.0f,0.0f);                                 //if needs to be stationary, stop the motor
+            }
+        }
+        
+        // Time will only = -1 if there is an error
+        else if(left_time==-1){
+            left_last_rpm = -999.0f;
+        } 
+        
+        // Print periodically
+        if(std::chrono::duration_cast<std::chrono::milliseconds>(left_print_timer.elapsed_time()).count()>=500){ //Was print_timer.read_ms()
+            printf("\033[2J\033[1;1H\r\n");
+            printf("****LEFT WHEEL SPEED****\r\n");
+            printf("Target: \t%4.3frpm\r\n",left_target_rpm);
+            printf("Current:\t%4.3frpm\r\n",left_last_rpm);
+            left_print_timer.reset();
+        } 
+
+        // A delay can be used to set how often the loop runs
+        ThisThread::sleep_for(std::chrono::milliseconds(loop_delay_ms));
+    }
+}
+
+void rightspeedcontroller(){
+
+    
+    float right_target_rpm = 40.0f;       // What rpm are we going to try to achieve?
+    float pwm_increment = 0.003f;   // This value affects how quickly we can reach the target but also the stability of our controller
+    int loop_delay_ms = 1;          // This sets how often the loop runs
+    int ppr = 20;//left_encoder.getPulsesPerRotation();      // To work out how fast we are going we need to know how many pulses are in a complete rotation
+    
+    
+    Timer right_print_timer;
+    right_print_timer.start();
+
+    float right_last_rpm = 0.0f;
+    while(1){
+        int32_t right_time = right_encoder.getLastPulseTimeUs(); //check pulse
+        if(right_time>0){ //if pulse received
+            float right_rpm = (60000000.0f/(ppr*right_time));               //calculate rpm
+            float err = right_target_rpm-right_rpm;                         //calculate error rate
+            if(right_target_rpm==0.0f){           //check if target rpm is 0
+                Wheel.Speed(0.0f ,0.0f);     // If so stop the motors 
+            }
+            else{                                        //alter speed
+                if(right_rpm < (right_target_rpm)){
+                    Wheel.Speed(Wheel.getSpeedRight()+pwm_increment,0.0f);
+                }
+                else if(right_rpm > right_target_rpm){
+                    Wheel.Speed(Wheel.getSpeedRight()-pwm_increment,0.0f);
+                }
+            }  
+
+            right_last_rpm = right_rpm;
+        }
+ 
+        // Check to see if the wheel IS stationary
+        else if(right_time==-2){                      //check if wheel is stationary
+            right_last_rpm = 0.0f;        
+            if(right_target_rpm !=0.0f){
+                Wheel.Speed(Wheel.getSpeedRight()+pwm_increment,0.0f);   //if stationary but needs to move increase speed
+            }
+            else{
+                Wheel.Speed(0.0f,0.0f);                                 //if needs to be stationary, stop the motor
+            }
+        }
+        
+        // Time will only = -1 if there is an error
+        else if(right_time==-1){
+            right_last_rpm = -999.0f;
+        } 
+        
+        // Print periodically
+        if(std::chrono::duration_cast<std::chrono::milliseconds>(right_print_timer.elapsed_time()).count()>=500){ //Was print_timer.read_ms()
+            printf("\033[2J\033[1;1H\r\n");
+            printf("****RIGHT WHEEL SPEED****\r\n");
+            printf("Target: \t%4.3frpm\r\n",right_target_rpm);
+            printf("Current:\t%4.3frpm\r\n",right_last_rpm);
+            right_print_timer.reset();
+        } 
+
+        // A delay can be used to set how often the loop runs
+        ThisThread::sleep_for(std::chrono::milliseconds(loop_delay_ms));
+    }
+}
+
+
+
+
+
+
 
 int main ()
 {
@@ -54,53 +189,54 @@ int main ()
     left_encoder.start();
     right_encoder.start();
 
-  
-    /*********************ENCODER TESTS***************/
-    // These contain while(1) loops so ensure that they are removed or commented out when running your own code
-    // If these lines are left in the lines below will never run
-    // These are examples only, and not all functionality within them is required for your coursework
-    // HINT: you don't need to measure speed
-    // Check PROJ100_Encoder_Tests.cpp for the code for these functions
-    /*************************************************/
-
-    // simplest_test();
-    // simple_test();
-    rotations_test();
-    // ave_pulse_time_test();
-    // speed_test();
-    
-
 
     //*******Demostration of the pwm_tune library***********//
-    jingle_bells(2);
-    close_encounter(2);
+    //jingle_bells(2);
+   // close_encounter(2);
 
 
 
     while(true){
 
-
-        // The following lines demostrate the use of the microswitches.
-        // You should remove these from your final solution 
-        // You could maybe find a better use for the switches(collision detection?)
-        if(microswitch1.read()==1){
-            // If the first switch is pressed print Hello
-            printf("Hello\n");
+        Wheel.Speed(-0.6f,-0.6f);
+        
+        if(microswitch1.read()==1){ //LEFT
+            
+            switch1 = true;
         }
-        if(microswitch2.read()==1){
-            // If the second switch is pressed print Goodbye
-            printf("Goodbye\n");
+        else{
+            switch1 = false;
+        }
+        if(microswitch2.read()==1){ //RIGHT
+            
+            switch2 = true;
+        }
+        else{
+            switch2 = false;
+        }
+        if((switch1 == true)and(switch2 == true)){ //no angle, full stop
+            printf("Stop\n");
+            Wheel.Speed(0.0f,0.0f);
+        }
+        if((switch1 == true)and(switch2 == false)){ //angled to the left
+            printf("Pointing left\n");
+            Wheel.Speed(-0.6f,0.0f);
+        }
+        if((switch1 == false)and(switch2 == true)){ //angled to the right
+            printf("Pointing right\n");
+            Wheel.Speed(0.0f,-0.6f);
         }
         wait_us(500000);
 
-
+        
 
         // Write the parts of your code which should run in a loop between here..
         
+        
+
+           
 
 
-
-        // ..and here
-
-    };
+        
+    }
 }
